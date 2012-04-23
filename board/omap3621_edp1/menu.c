41e600f5b925fdf48451b02f902949c2b427d04e
gmarkall <grm08@doc.ic.ac.uk>
2012-04-23 01:20:31 +0100

    Run astyle on menu.c because it's making me go cross-eyed.

--- board/omap3621_edp1/menu.c
+++ board/omap3621_edp1/menu.c
@@ -42,128 +42,148 @@
 
 
 // FIXME: Find out what this is.
-char read_u_boot_device(void) { return 'X'; }
+char read_u_boot_device(void) {
+    return 'X';
+}
 
 // FIXME: Implement this properly
-int gpio_read(int gpio) { return 0; }
+int gpio_read(int gpio) {
+    return 0;
+}
 
 // -----------------------------------
 
 
 int do_menu() {
 
-	// FIXME: Should this fn be implemented?
-        //lcd_bl_set_brightness(255);
-	//lcd_console_init();
+    // FIXME: Should this fn be implemented?
+    //lcd_bl_set_brightness(255);
+    //lcd_console_init();
 
 #define NUM_OPTS		8  //number of boot options
 
-char *opt_list[NUM_OPTS] = 	{" Internal eMMC Normal     ",
-				 " Internal eMMC Recovery   ",
-				 " Internal eMMC Alternate  ",
-				 " SD Card Normal           ",
-				 " SD Card Recovery         ",
-				 " SD Card Alternate        ",
-				 " Start fastboot           ",
-				 " default boot from:  ",
-				};
+    char *opt_list[NUM_OPTS] = 	{" Internal eMMC Normal     ",
+                                 " Internal eMMC Recovery   ",
+                                 " Internal eMMC Alternate  ",
+                                 " SD Card Normal           ",
+                                 " SD Card Recovery         ",
+                                 " SD Card Alternate        ",
+                                 " Start fastboot           ",
+                                 " default boot from:  ",
+                                };
 
-		int x;
-		int cursor = 0;
-		u8 pwron = 0;
-		int ignore_last_option = 1;  // assume file is missing
+    int x;
+    int cursor = 0;
+    u8 pwron = 0;
+    int ignore_last_option = 1;  // assume file is missing
 
-        // FIXME: What is this?
-        //if (twl6030_hw_status(&pwron)) {
-        //        lcd_console_setpos(MENUTOP, 2);
-        //        lcd_puts("Error: Failed to read twl6030 hw_status\n");
-        //}
+    // FIXME: What is this?
+    //if (twl6030_hw_status(&pwron)) {
+    //        lcd_console_setpos(MENUTOP, 2);
+    //        lcd_puts("Error: Failed to read twl6030 hw_status\n");
+    //}
 
-		if (read_u_boot_device() != 'X') // if that file is there
-			{ ignore_last_option = 0;};
+    if (read_u_boot_device() != 'X') // if that file is there
+    {
+        ignore_last_option = 0;
+    };
 
-		lcd_console_setcolor(CONSOLE_COLOR_CYAN, CONSOLE_COLOR_BLACK);
-		// lcd_clear (NULL, 1, 1, NULL);
-		lcd_console_setpos(MENUTOP-3, INDENT);
-			lcd_puts(" Boot Menu");
-		lcd_console_setpos(MENUTOP-2, INDENT);
-			lcd_puts(" -------------");
+    lcd_console_setcolor(CONSOLE_COLOR_CYAN, CONSOLE_COLOR_BLACK);
+    // lcd_clear (NULL, 1, 1, NULL);
+    lcd_console_setpos(MENUTOP-3, INDENT);
+    lcd_puts(" Boot Menu");
+    lcd_console_setpos(MENUTOP-2, INDENT);
+    lcd_puts(" -------------");
 
-		for (x=0; x < (NUM_OPTS - ignore_last_option); x++) {
-		    lcd_console_setpos(MENUTOP+x, INDENT);
-	            lcd_puts(opt_list[x]);
-                   }
-		if (ignore_last_option == 0) {
-			if (read_u_boot_device() == '1') {
-				lcd_puts("EMMC ");
-				} else {
-				lcd_puts("SD   "); }
-		}
-		lcd_console_setpos(MENUTOP+10, INDENT);
-			lcd_puts(" \"n\" moves to next item");
-		lcd_console_setpos(MENUTOP+11, INDENT);
-			lcd_puts(" POWER button selects");
-		lcd_console_setpos(60, 0);
-			lcd_puts(" ------\n Menu by j4mm3r.\n Redone by fattire - ALPHA (" __TIMESTAMP__  ") - ** EXPERIMENTAL **");
+    for (x=0; x < (NUM_OPTS - ignore_last_option); x++) {
+        lcd_console_setpos(MENUTOP+x, INDENT);
+        lcd_puts(opt_list[x]);
+    }
+    if (ignore_last_option == 0) {
+        if (read_u_boot_device() == '1') {
+            lcd_puts("EMMC ");
+        } else {
+            lcd_puts("SD   ");
+        }
+    }
+    lcd_console_setpos(MENUTOP+10, INDENT);
+    lcd_puts(" \"n\" moves to next item");
+    lcd_console_setpos(MENUTOP+11, INDENT);
+    lcd_puts(" POWER button selects");
+    lcd_console_setpos(60, 0);
+    lcd_puts(" ------\n Menu by j4mm3r.\n Redone by fattire - ALPHA (" __TIMESTAMP__  ") - ** EXPERIMENTAL **");
 
-		cursor=0;
+    cursor=0;
 
-		// highlight first option
-		lcd_console_setcolor(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_CYAN);
-		lcd_console_setpos(MENUTOP, INDENT);
-	        lcd_puts(opt_list[0]);
+    // highlight first option
+    lcd_console_setcolor(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_CYAN);
+    lcd_console_setpos(MENUTOP, INDENT);
+    lcd_puts(opt_list[0]);
 
-                int key;
+    int key;
 
-		// FIXME: Reimplement this for A1 power button
-                //do {udelay(100);} while (gpio_read(HOME_BUTTON) == 0) ;  // wait for release
+    // FIXME: Reimplement this for A1 power button
+    //do {udelay(100);} while (gpio_read(HOME_BUTTON) == 0) ;  // wait for release
 
-		do {
-		if ((key = twl4030_keypad_read_volume_key()) != 0) // button is pressed
-		   {
-			// unhighlight current option
-			lcd_console_setcolor(CONSOLE_COLOR_CYAN, CONSOLE_COLOR_BLACK);
-			lcd_console_setpos(MENUTOP+cursor, INDENT);
-	       		lcd_puts(opt_list[cursor]);
-			if (cursor == CHANGE_BOOT_DEV) {
-				if (read_u_boot_device() == '1') {
-					lcd_puts("EMMC ");
-					} else {
-					lcd_puts("SD   "); }
-				}
-			cursor++;
-			if (cursor == NUM_OPTS - ignore_last_option) cursor = 0;
-			// highlight new option
-			lcd_console_setcolor(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_CYAN);
-			lcd_console_setpos(MENUTOP+cursor, INDENT);
-			lcd_puts(opt_list[cursor]);
-			if (cursor == CHANGE_BOOT_DEV) {
-					if (read_u_boot_device() == '1') {
-					lcd_puts("EMMC ");
-					} else {
-					lcd_puts("SD   "); }
-				}
-			do {udelay(100);} while (twl4030_keypad_read_volume_key() != 0);  //wait for release
+    do {
+        if ((key = twl4030_keypad_read_volume_key()) != 0) // button is pressed
+        {
+            // unhighlight current option
+            lcd_console_setcolor(CONSOLE_COLOR_CYAN, CONSOLE_COLOR_BLACK);
+            lcd_console_setpos(MENUTOP+cursor, INDENT);
+            lcd_puts(opt_list[cursor]);
+            if (cursor == CHANGE_BOOT_DEV) {
+                if (read_u_boot_device() == '1') {
+                    lcd_puts("EMMC ");
+                } else {
+                    lcd_puts("SD   ");
+                }
+            }
+            cursor++;
+            if (cursor == NUM_OPTS - ignore_last_option) cursor = 0;
+            // highlight new option
+            lcd_console_setcolor(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_CYAN);
+            lcd_console_setpos(MENUTOP+cursor, INDENT);
+            lcd_puts(opt_list[cursor]);
+            if (cursor == CHANGE_BOOT_DEV) {
+                if (read_u_boot_device() == '1') {
+                    lcd_puts("EMMC ");
+                } else {
+                    lcd_puts("SD   ");
+                }
+            }
+            do {
+                udelay(100);
+            }
+            while (twl4030_keypad_read_volume_key() != 0);    //wait for release
 
-		   }
+        }
 
-		if ((gpio_read(POWER_BUTTON) == 1) && (cursor == CHANGE_BOOT_DEV)) {  //selected last option
-			if (read_u_boot_device() == '1') {write_u_boot_device('0');}
-				else {write_u_boot_device('1'); }
-			udelay(1000 *1000);
-			lcd_console_setcolor( CONSOLE_COLOR_BLACK, CONSOLE_COLOR_GREEN);
-			lcd_console_setpos(MENUTOP+CHANGE_BOOT_DEV, INDENT);
-			lcd_puts(opt_list[CHANGE_BOOT_DEV]);
-			if (read_u_boot_device() == '1') {
-					lcd_puts("EMMC ");
-					} else {
-					lcd_puts("SD   "); }
-			do {udelay(100);} while (gpio_read(POWER_BUTTON) == 1);  //wait for release
-		}
-			udelay(100);
-		} while ((gpio_read(POWER_BUTTON) == 0) || (cursor == CHANGE_BOOT_DEV));  // power button to select
-		lcd_console_setcolor( CONSOLE_COLOR_BLACK, CONSOLE_COLOR_GREEN);
-		lcd_console_setpos(MENUTOP+cursor, INDENT);
-		lcd_puts(opt_list[cursor]);
-		return cursor;
+        if ((gpio_read(POWER_BUTTON) == 1) && (cursor == CHANGE_BOOT_DEV)) {  //selected last option
+            if (read_u_boot_device() == '1') {
+                write_u_boot_device('0');
+            }
+            else {
+                write_u_boot_device('1');
+            }
+            udelay(1000 *1000);
+            lcd_console_setcolor( CONSOLE_COLOR_BLACK, CONSOLE_COLOR_GREEN);
+            lcd_console_setpos(MENUTOP+CHANGE_BOOT_DEV, INDENT);
+            lcd_puts(opt_list[CHANGE_BOOT_DEV]);
+            if (read_u_boot_device() == '1') {
+                lcd_puts("EMMC ");
+            } else {
+                lcd_puts("SD   ");
+            }
+            do {
+                udelay(100);
+            }
+            while (gpio_read(POWER_BUTTON) == 1);    //wait for release
+        }
+        udelay(100);
+    } while ((gpio_read(POWER_BUTTON) == 0) || (cursor == CHANGE_BOOT_DEV));  // power button to select
+    lcd_console_setcolor( CONSOLE_COLOR_BLACK, CONSOLE_COLOR_GREEN);
+    lcd_console_setpos(MENUTOP+cursor, INDENT);
+    lcd_puts(opt_list[cursor]);
+    return cursor;
 }
